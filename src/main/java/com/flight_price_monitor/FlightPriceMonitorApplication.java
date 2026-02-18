fc7c15cd829f4a466d0525bbc91ed4fc5a588f13
Mateusz Doktor <matdoktor3@gmail.com>
2026-02-18 17:42:21 +0100

    initial commit

---
+++ src/main/java/com/flight_price_monitor/FlightPriceMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.flight_price_monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightPriceMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightPriceMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flight_price_monitor/FlightPriceMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flight_price_monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightPriceMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
